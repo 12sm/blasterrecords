1e34c5289c9446160b662c9325475802fd766567
Ian Noyes <ian@12southmusic.com>
2014-04-24 12:51:29 -0500

    add trimshortcode

--- functions.php
+++ functions.php
@@ -35,8 +35,21 @@ jQuery(document).ready(function() {
 });
 </script>
 
+
+add_shortcode('trim', 'trim_shortcode');
+function trim_shortcode($atts, $content = '') {
+  $content = wpv_do_shortcode($content);
+  $length = (int)$atts['length'];
+  if (strlen($content) > $length) {
+    $content = substr($content, 0, $length) . '&hellip;';
+  }
+  return $content;
+}
+
 <?php
 }
+
+
 
 /**
  * Roots includes
