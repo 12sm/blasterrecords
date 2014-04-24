218594153b5ff8a38bd46d9c13a6fc6ee056ae34
Ian Noyes <ian@12southmusic.com>
2014-04-24 14:56:33 -0500

    remove trim

--- functions.php
+++ functions.php
@@ -36,17 +36,6 @@ jQuery(document).ready(function() {
 </script>
 
 <?php
-add_shortcode('trim', 'trim_shortcode');
-function trim_shortcode($atts, $content = '') {
-  $content = wpv_do_shortcode($content);
-  $length = (int)$atts['length'];
-  if (strlen($content) > $length) {
-    $content = substr($content, 0, $length) . '&hellip;';
-  }
-  return $content;
-} ?>
-
-<?php
 }
 
 
