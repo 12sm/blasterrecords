488aca7fb05e415e5691fe5ff67b105d2f5cd63b
Ian Noyes <ian@12southmusic.com>
2014-05-05 14:04:17 -0500

    finish debuging the player

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -25,7 +25,6 @@ if ( is_front_page() || is_page('artists')) {
 
       <?php if (is_singular( 'artist' ) ) {
         $postid = get_the_ID();
-        echo $postid;
         echo do_shortcode('[wpv-view name="output-album-meta" wpvprchildof='.$postid.' ]');
       } else{
         echo "not artist";
