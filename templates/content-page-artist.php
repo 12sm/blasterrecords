88af05c3a251bfbabb74a172943a1438e5592bcf
Ian Noyes <ian@12southmusic.com>
2014-05-05 17:52:20 -0500

    artist page artists

--- templates/content-page-artist.php
+++ templates/content-page-artist.php
@@ -3,4 +3,4 @@
 		<?php echo do_shortcode('[wpv-view name="homeartist-page-headline"]'); ?>
 	</div>
 </div>
-<?php echo do_shortcode('[wpv-view name="home-page-artists"]'); ?>
+<?php echo do_shortcode('[wpv-view name="artist-page-artists"]'); ?>
