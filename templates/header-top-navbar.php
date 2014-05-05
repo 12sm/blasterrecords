b25c84a047edf7a3b30c5908bfbc5552ace42702
Ian Noyes <ian@12southmusic.com>
2014-05-05 16:21:16 -0500

    output track meta

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -25,7 +25,7 @@ if ( is_front_page() || is_page('artists')) {
 
       <?php if (is_singular( 'artist' ) ) {
         $postid = get_the_ID();
-        echo do_shortcode('[wpv-view name="output-album-meta" wpvprchildof='.$postid.' ]');
+        echo do_shortcode('[wpv-view name="output-track-meta" wpvprchildof='.$postid.' ]');
       } else{
         echo "not artist";
       $postid = get_the_ID();
