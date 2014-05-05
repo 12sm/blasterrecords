542f7d8d2e118f04ceaf224c34d781ee348c80ec
Ian Noyes <ian@12southmusic.com>
2014-05-05 17:42:06 -0500

    content templates

--- template-artist.php
+++ template-artist.php
@@ -4,5 +4,4 @@ Template Name: Artist Template
 */
 ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', 'page-home'); ?>
+<?php get_template_part('templates/content', 'page-artist'); ?>
---
+++ templates/content-page-artist.php
@@ -0,0 +1,6 @@
+<div class="row">
+	<div class="col-sm-12 headline">
+		<?php echo do_shortcode('[wpv-view name="homeartist-page-headline"]'); ?>
+	</div>
+</div>
+<?php echo do_shortcode('[wpv-view name="home-page-artists"]'); ?>
