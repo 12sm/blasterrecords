4707a2240506e69700fd7cf04def0a871ca6fa25
Ian Noyes <ian@12southmusic.com>
2014-05-05 12:51:50 -0500

    syntax

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,7 @@ var Roots = {
             audio.load($('a', next).attr('data-src'));
             audio.play();
       }
+      });
       // Load in the first track
         var audio = a[0];
             first = $('ol a').attr('data-src');
