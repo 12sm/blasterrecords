02b2c0aab41fbbcf6ddb869136ab60d01e307c91
Ian Noyes <ian@12southmusic.com>
2014-05-05 13:08:19 -0500

    cols and nav stuff

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -6,8 +6,6 @@ if ( is_front_page() || is_page('artists')) {
     } ?>
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
-  <div class="row">
-        <div class="col-sm-7">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
@@ -17,8 +15,6 @@ if ( is_front_page() || is_page('artists')) {
       </button>
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
-    </div>
-    <div class="col-sm-5">
     <nav class="collapse navbar-collapse" role="navigation">
       
       <?php
@@ -26,8 +22,7 @@ if ( is_front_page() || is_page('artists')) {
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
         endif;
       ?>
-      </div>
-      <div class="col-sm-5">
+
       <?php if (is_singular( 'artist' ) ) {
         echo do_shortcode('[wpv-view name="output-album-meta" wpvprchildof='.$postid.' ]');
       } else{
@@ -36,9 +31,7 @@ if ( is_front_page() || is_page('artists')) {
       }
       ?>
       </div>
-      </div>
     </nav>
-  </div>
   <?php
 
 if ( is_front_page() ) {    
