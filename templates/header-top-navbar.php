711bfc38571935fad3248d706e06047dde278069
Ian Noyes <ian@12southmusic.com>
2014-05-21 14:53:43 -0500

    debug donezo kind of

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -19,14 +19,10 @@
       ?>
       
 	  <div class="player col-sm-12 col-md-4 col-lg-3">
-    <h1>commits even appearing?</h1>
 	      <?php if (is_singular( 'artist' ) ) {
 	        $postid = get_the_ID();
-          echo "artist page";
-          echo $postid;
 	        echo do_shortcode('[wpv-view name="output-track-meta" wpvprchildof='.$postid.' ]');
 	      } else{
-          echo "not artist page";
 	      echo do_shortcode('[wpv-view name="output-track-meta"]'); 
 	      }
 	      ?>
