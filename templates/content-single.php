e7a6ee35aad7e2617635478bfd2e18fdec94d5fa
Ian Noyes <ian@12southmusic.com>
2014-06-10 17:06:41 -0500

    cheap css fix

--- templates/content-single.php
+++ templates/content-single.php
@@ -9,10 +9,10 @@
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
       
       <nav class="post-nav">  
-	      <!--<ul class="pager">
+	     <ul class="pager">
 	      	<li class="previous"><?php previous_post_link( '%link', '&larr; %title' ); ?></li>
 	      	<li class="next"><?php next_post_link( '%link', '%title &rarr;' ); ?></li>
-	      </ul>-->
+	      </ul>
 	  </nav>
       
     </footer>
