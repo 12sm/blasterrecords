816852b0e3dbcb9465190823c6a5890a34167a2c
Ian Noyes <ian@12southmusic.com>
2014-06-10 17:04:46 -0500

    you?

--- templates/content-single.php
+++ templates/content-single.php
@@ -6,13 +6,13 @@
       <?php the_content(); ?>
     </div>
     <footer>
-      <!--<?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>-->
+      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
       
       <nav class="post-nav">  
-	      <ul class="pager">
+	      <!--<ul class="pager">
 	      	<li class="previous"><?php previous_post_link( '%link', '&larr; %title' ); ?></li>
 	      	<li class="next"><?php next_post_link( '%link', '%title &rarr;' ); ?></li>
-	      </ul>
+	      </ul>-->
 	  </nav>
       
     </footer>
